31bb161ac9b913533c26ff0369755d1f34c261af
Joel-Gordon <Joel.Gordon@coxautoinc.com>
2020-07-11 23:08:07 -0500

    I want to take mode out for now(will fix later)

--- vivint.py
+++ vivint.py
@@ -309,8 +309,7 @@ class VivintCloudSession(object):
             request_kwargs = dict(
                 method="PUT",
                 url="http://localhost:8080/api/zone/1/config",
-                body=json.dumps({"mode":current_state.get("mode"),
-                    "coolSetPoint":current_state.get("cooling_setpoint"),
+                body=json.dumps({"coolSetPoint":current_state.get("cooling_setpoint"),
                 "heatSetPoint":current_state.get("heating_setpoint")}).encode("utf-8"),
                 headers={
                     "Content-Type":
