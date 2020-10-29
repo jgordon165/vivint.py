823bafc2bba415c47453cb60203a13748c61675a
Joel-Gordon <Joel.Gordon@coxautoinc.com>
2020-10-29 16:47:19 -0500

    pretty sure this is my problem

--- demoswitch.py
+++ demoswitch.py
@@ -61,7 +61,8 @@ while True:
         state = sensor.sensor_state()
 
         if state.get("name") == sensor_one_name:
-            sensor_one_secs = (timestamp_conv_factor - (state.get("activitytime") - sensor_one_state).total_seconds())
+            sensor_one_secs = (timestamp_conv_factor - (sensor_one_state - state.get("activitytime")).total_seconds())
+            print(sensor_one_secs)
             if sensor_one_secs < motion_duration_in_seconds:
                 sensor_one_state = state.get("activitytime") 
                 switch_one_turn_on = True
@@ -81,7 +82,7 @@ while True:
             elif switch_one_turn_off == True and switch_one_state != 0:
                 switch_one.set_switch(0)
         if state.get("name") == sensor_two_name:
-            sensor_two_secs = (timestamp_conv_factor - (state.get("activitytime") - sensor_two_state).total_seconds())
+            sensor_two_secs = (timestamp_conv_factor - (sensor_two_state - state.get("activitytime")).total_seconds())
             if sensor_two_secs < motion_duration_in_seconds:
                 sensor_two_state = state.get("activitytime")
                 switch_two_turn_on = True
