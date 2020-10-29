e2b3abf1c132fbee829bd951d056fce1e57c40bf
Joel-Gordon <Joel.Gordon@coxautoinc.com>
2020-10-29 14:08:45 -0500

    enhanced debugging

--- demoswitch.py
+++ demoswitch.py
@@ -31,9 +31,6 @@ sensors = panels[0].get_devices(device_type_set=[
 while True:
 
     for multiswitch in multiswitches:
-        print(
-            "Getting state of multiswitch %d on panel %d" %
-            (multiswitch.id(), panels[0].id()))
         state = multiswitch.multi_swtich_state()
 
         if state.get("name") == switch_one_name:
@@ -45,20 +42,18 @@ while True:
 
     
     for sensor in sensors:
-        print(
-            "Getting state of sensors %d on panel %d" %
-            (sensor.id(), panels[0].id()))
         state = sensor.sensor_state()
 
         if state.get("name") == sensor_one_name:
             sensor_one_state = state.get("active") 
+            print("sensor 1")
+            print(sensor_one_name)
         if state.get("name") == sensor_two_name:
             sensor_two_state = state.get("active")
+            print("sensor 2")
+            print(sensor_two_state)
 
     if sensor_one_state == True and switch_one_state == "0":
         switch_one.set_switch(switch_one_default_level)
     if sensor_two_state == True and switch_two_state == "0":
         switch_two.set_switch(switch_two_default_level)
-
-    #sleep 2 seconds
-    time.sleep(2)
